ddaed17cef44ad2d8147c11812f8f60e0a102ee1
diler <dilersingh96@yahoo.com>
2018-11-26 00:17:48 +0530

    real commit one

--- firebase_automate.py
+++ firebase_automate.py
@@ -1,3 +1,16 @@
-print('hi i am from window')
-print('hello again')
-print('i am in mac')
+import requests
+from bs4 import BeautifulSoup
+# <ul class="videoInfoList">
+# 	<li><a href="https://www.youtube.com/watch?v=kQHuiTDTvYk" target="_break">YouTube <i class="icon-external-link"></i></a></li>
+# </ul>
+
+url='https://imvdb.com/new/'
+header={'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
+response= requests.get(url,header)
+soup=BeautifulSoup(response.content,'html.parser')
+songs_list=soup.find('div',class_='rack_node')
+if response.status_code!=200:
+    print(response.status_code)
+else:
+    x=songs_list.findNext('a')
+    print(x['href'])
