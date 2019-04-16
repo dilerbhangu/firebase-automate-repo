053f7da7056f134ed1abb956cfe22be2641974ee
diler <dilersingh96@yahoo.com>
2019-04-16 19:48:03 +0530

    commit

--- firebase_automate_hindi.py
+++ firebase_automate_hindi.py
@@ -4,64 +4,70 @@ import time
 from tqdm import tqdm
 import passwords
 import pyrebase
+import firebase_conn_hindi
 
-config={"apiKey": passwords.API_KEY,
-        "authDomain":passwords.AUTH_DOMAIN,
-        "databaseURL":passwords.DATABASE_URL,
-        "storageBucket":passwords.STROAGE_BUCKET
-}
-timestamp=int(time.time())
+config = {"apiKey": passwords.API_KEY,
+          "authDomain": passwords.AUTH_DOMAIN,
+          "databaseURL": passwords.DATABASE_URL,
+          "storageBucket": passwords.STROAGE_BUCKET
+          }
+timestamp = int(time.time())
 
 with open('youtube_code_hindi.txt') as f:
-    youtube_code_previous=f.read().splitlines()[0]
+    youtube_code_previous = f.read().splitlines()[0]
 
-url='https://www.mrhd.in/hindi/'
+url = 'https://www.mrhd.in/hindi/'
 try:
-    response=requests.get(url,timeout=10)
+    response = requests.get(url, timeout=10)
 except requests.exceptions.RequestException as e:
     print(e)
 else:
-        soup=BeautifulSoup(response.content,'html.parser')
-        songs_list=soup.find_all('div',class_='mh-loop-thumb')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    songs_list = soup.find_all('div', class_='mh-loop-thumb')
 
-        songs_links=[]
-        d=[]
-        t=[]
+    songs_links = []
+    d = []
+    t = []
 
-        for song in tqdm(songs_list):
-            songs_links.append(song.findNext('a')['href'])
+    for song in tqdm(songs_list):
+        songs_links.append(song.findNext('a')['href'])
 
-        for link in tqdm(songs_links):
-            for i in range(10):
-                try:
-                    response=requests.get(link,timeout=30)
-                except requests.exceptions.RequestException as e:
-                    print(e)
-                    time.sleep(5)
-                else:
-                    response=response
-                    break
+    for link in tqdm(songs_links):
+        for i in range(10):
+            try:
+                response = requests.get(link, timeout=30)
+            except requests.exceptions.RequestException as e:
+                print(e)
+                time.sleep(5)
+            else:
+                response = response
+                break
 
-            if response.status_code==200:
-                soup=BeautifulSoup(response.content,'html.parser')
-                video_info=soup.find('iframe')['src']
-                video_info=video_info.split('//www.youtube.com/embed/',1)[1]
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            video_info = soup.find('iframe')['src']
+            video_info = video_info.split('//www.youtube.com/embed/', 1)[1]
 
-                video_title=soup.find('h1',class_='entry-title').get_text()
-                video_title=video_title.split(' Video Download HD',1)
+            video_title = soup.find('h1', class_='entry-title').get_text()
+            video_title = video_title.split(' Video Download HD', 1)
 
-                if video_info==youtube_code_previous:
-                    break
+            if video_info == youtube_code_previous:
+                break
 
-                t.append(video_info)
-                d.append(video_title[0])
+            t.append(video_info)
+            d.append(video_title[0])
 
 
-if len(d)>0:
-    with open('youtube_code_hindi.txt','w') as f:
+if len(d) > 0:
+    with open('youtube_code_hindi.txt', 'w') as f:
         for item in t:
-            f.write('%s\n'%item)
+            f.write('%s\n' % item)
 
-    with open('youtube_title_hindi.txt','w') as f:
+    with open('youtube_title_hindi.txt', 'w') as f:
         for item in d:
-            f.write('%s\n'%item)
+            f.write('%s\n' % item)
+
+    firebase_conn_hindi.call()
+
+else:
+    print('No New Song to upload')
--- firebase_automate_punjabi.py
+++ firebase_automate_punjabi.py
@@ -4,29 +4,30 @@ import time
 from tqdm import tqdm
 import pyrebase
 import passwords
+import firebase_conn_punjabi
 
-config={"apiKey": passwords.API_KEY,
-        "authDomain":passwords.AUTH_DOMAIN,
-        "databaseURL":passwords.DATABASE_URL,
-        "storageBucket":passwords.STROAGE_BUCKET
-}
-timestamp=int(time.time())
+config = {"apiKey": passwords.API_KEY,
+          "authDomain": passwords.AUTH_DOMAIN,
+          "databaseURL": passwords.DATABASE_URL,
+          "storageBucket": passwords.STROAGE_BUCKET
+          }
+timestamp = int(time.time())
 
 with open('youtube_code_punjabi.txt') as f:
-    youtube_code_previous=f.read().splitlines()[0]
+    youtube_code_previous = f.read().splitlines()[0]
 
-url='https://www.mrhd.in/punjabi/'
+url = 'https://www.mrhd.in/punjabi/'
 try:
-    response=requests.get(url,timeout=10)
+    response = requests.get(url, timeout=10)
 except requests.exceptions.RequestException as e:
     print(e)
 else:
-    soup=BeautifulSoup(response.content,'html.parser')
-    songs_list=soup.find_all('div',class_='mh-loop-thumb')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    songs_list = soup.find_all('div', class_='mh-loop-thumb')
 
-    songs_links=[]
-    d=[]
-    t=[]
+    songs_links = []
+    d = []
+    t = []
 
     for song in tqdm(songs_list):
         songs_links.append(song.findNext('a')['href'])
@@ -34,33 +35,37 @@ else:
     for link in tqdm(songs_links):
         for i in range(10):
             try:
-                response=requests.get(link,timeout=30)
+                response = requests.get(link, timeout=30)
             except requests.exceptions.RequestException as e:
                 print(e)
                 time.sleep(5)
             else:
-                response=response
+                response = response
                 break
 
-        if response.status_code==200:
-            soup=BeautifulSoup(response.content,'html.parser')
-            video_info=soup.find('iframe')['src']
-            video_info=video_info.split('//www.youtube.com/embed/',1)[1]
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            video_info = soup.find('iframe')['src']
+            video_info = video_info.split('//www.youtube.com/embed/', 1)[1]
 
-            video_title=soup.find('h1',class_='entry-title').get_text()
-            video_title=video_title.split(' Video HD  Download',1)
+            video_title = soup.find('h1', class_='entry-title').get_text()
+            video_title = video_title.split(' Video HD  Download', 1)
 
-            if video_info==youtube_code_previous:
+            if video_info == youtube_code_previous:
                 break
             t.append(video_info)
             d.append(video_title[0])
 
+    if len(d) > 0:
+        with open('youtube_code_punjabi.txt', 'w') as f:
+            for item in t:
+                f.write('%s\n' % item)
 
-if len(d)>0:
-    with open('youtube_code_punjabi.txt','w') as f:
-        for item in t:
-            f.write('%s\n'%item)
+        with open('youtube_title_punjabi.txt', 'w') as f:
+            for item in d:
+                f.write('%s\n' % item)
 
-    with open('youtube_title_punjabi.txt','w') as f:
-        for item in d:
-            f.write('%s\n'%item)
+        firebase_conn_punjabi.call()
+
+    else:
+        print('No New Song to upload')
